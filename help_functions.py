b3bf7f50db9b252d65650c93e2ac29148da709c7
JorgenBakken <jorbak@hotmail.no>
2023-06-07 09:35:00 +0200

    Created an euler step function to solve ODEs

--- help_functions.py
+++ help_functions.py
@@ -10,3 +10,16 @@ def gamma_func(theta, beta, radius, dyC):
     '''
     alpha = 2 * np.arccos(np.cos(beta / 2) - 4 * (1 - np.cos(theta)) / (3 * np.pi))
     return 2 * np.arccos(np.cos(alpha / 2) + dyC / radius)
+
+
+#Need an ode solver 
+def euler_step(f, w_old, dt):
+    '''
+    Solve dw/dt = f(w), where w can be a vector
+    f     : function 
+    w_old : initial step 
+    dt    : step size 
+    w_new : new step
+    '''
+    w_new = w_old + dt * f(w_old)
+    return w_new
