00421d01f5e35c3c16f09ce6517bbc88f0ddf605
JorgenBakken <jorbak@hotmail.no>
2023-06-07 17:38:08 +0200

    Implemented simple version of ODE solver

--- help_functions.py
+++ help_functions.py
@@ -23,3 +23,21 @@ def euler_step(f, w_old, dt):
     '''
     w_new = w_old + dt * f(w_old)
     return w_new
+
+def solve_ODE(dt, t_0, t_end, w_0, f, step_function):
+    '''
+    dt : step length 
+    t_0: start time 
+    t_end: end time 
+    w_0: start step
+    num_step: number of steps
+    dt_actual: corrected to be able to seperate each time step in equidistant steps
+    w_matrix : a matrix containing all the time steps of w
+    '''
+    num_steps = find_num_steps(t_end,t_0,dt)
+    dt_actual = find_actual_dt(t_end,t_0,dt,num_steps)
+    w_matrix = np.zeros(num_steps, len(w_0))
+    w_matrix[0,:] = w_0
+    for i in range(0, num_steps - 1):
+        w_matrix[i + 1] = step_function(f, w_matrix[i], dt_actual)
+    return w_matrix
