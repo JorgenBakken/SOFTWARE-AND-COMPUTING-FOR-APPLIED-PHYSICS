ec75f02915d881810feee7eb78f96ee0d28cf60b
JorgenBakken <jorbak@hotmail.no>
2023-06-10 16:33:05 +0200

    Updated gamma_func

--- help_functions.py
+++ help_functions.py
@@ -1,16 +1,21 @@
 import numpy as np
 
-#Will need a sector angle gamma for calculations
 def gamma_func(theta, beta, radius, dyC):
     '''
+    Calculates the sector angle gamma
+
     theta: rotational angle 
     beta: sector angle
     radius: radius of ship (ship is assumed to a half circle)
     dyC: vertical displacement 
     '''
-    alpha = 2 * np.arccos(np.cos(beta / 2) - 4 * (1 - np.cos(theta)) / (3 * np.pi))
-    return 2 * np.arccos(np.cos(alpha / 2) + dyC / radius)
+    arccos_term = np.cos(beta / 2) - 4 * (1 - np.cos(theta)) / (3 * np.pi)
+    assert -1 <= arccos_term <= 1, "Value of 'a' is outside the range (-1, 1)"
+    
+    alpha = 2 * np.arccos(arccos_term)
+    assert -1 <= alpha <= 1, "Value of 'alpha' is outside the range (-1, 1)"
 
+    return 2 * np.arccos(np.cos(alpha / 2) + dyC / radius)
 
 #Need an ode solver 
 def euler_step(f, w_old, dt):
