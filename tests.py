7dd254677038b1628debe84e7601c6cd1b34dff1
JorgenBakken <jorbak@hotmail.no>
2023-06-10 17:36:29 +0200

    Implemented test_find_num_steps

--- tests.py
+++ tests.py
@@ -32,4 +32,26 @@ def test_gamma_func(theta, beta, radius, dyC_factor):
     gamma = hf.gamma_func(theta, beta, radius, dyC)
     assert isinstance(gamma, float), "Gamma should be a float value"
 
+@given(t_0 = st.floats(min_value=-10000000,max_value=10000000), 
+       t_end_extra = st.floats(min_value=0.000001, max_value=10000000), 
+       dt = st.floats(min_value= 0.000001, max_value=1000000))
+def test_find_num_steps(t_0 ,t_end_extra, dt):
+    '''
+    Tests the find_num_steps to make sure it returns a valid number of steps
+    It should either be the number of steps required to get to the end time, or past it
+
+    Arguments 
+    t_0         : start time 
+    t_end_extra : distance from t_0 to t_end, to make sure t_end comes after t_0
+    t_end       : end time
+    dt          : step size 
+    '''
+    t_end = t_0 + t_end_extra
+    num_steps = hf.find_num_steps(t_0 ,t_end, dt)
+    print(":")
+    print(num_steps)
+    print(dt)
+    print(t_end_extra)
+    assert isinstance(num_steps, int), "Number of steps should be an integer"
+    assert t_0 + num_steps * dt >= t_end, "Should be able to reach the end"
 
