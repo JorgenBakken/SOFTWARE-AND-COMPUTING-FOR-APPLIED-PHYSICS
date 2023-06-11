ed360ad2936ddf14ed31a627d7889f1b5ed2de9c
JorgenBakken <jorbak@hotmail.no>
2023-06-11 10:03:30 +0200

    Updated test_solve_ODE_shape_t_array

--- tests.py
+++ tests.py
@@ -103,7 +103,7 @@ def test_euler_step(t, w_old, dt):
 
 @given(dt    = st.floats(min_value=0.000001, max_value=10000000),
        t_0   = st.floats(min_value=-10000000, max_value=10000000),
-       t_end_factor = st.floats(min_value= 0.000001, max_value=10),
+       t_end_factor = st.floats(min_value= 0.1, max_value=10),
        w_0   = st.lists(st.floats(min_value=-10000000, max_value=10000000),min_size=2, max_size=2))
 @settings(max_examples=50)
 def test_solve_ODE_shape_t_array(dt, t_0, t_end_factor, w_0):
@@ -128,8 +128,7 @@ def test_solve_ODE_shape_t_array(dt, t_0, t_end_factor, w_0):
     
     t_array, w_matrix = hf.solve_ODE(dt, t_0, t_end, w_0, f, hf.euler_step)
     num_steps = hf.find_num_steps(t_0 ,t_end, dt)
-    new_df = hf.find_actual_dt(t_0, t_end, num_steps)
-    assert t_array.shape[0] == np.ceil((t_end - t_0) / new_df)
+    assert t_array.shape[0] == num_steps
 
 @given(dt    = st.floats(min_value=0.000001, max_value=10000000),
        t_0   = st.floats(min_value=-10000000, max_value=10000000),
