09bab16c808ba607805c4d5e466e66ae34ea9c4f
JorgenBakken <jorbak@hotmail.no>
2023-06-13 17:03:03 +0200

    Moved run of plot_small_angle_approx to main

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import help_functions as hf
+import matplotlib.pyplot as plt
+
+import ship_variables as sv
+import ship_variables as gv
+import plotting
+
+# Plotting the comparison between the full equation and small angle approximation for a simple harmonic oscillator
+plotting.plot_small_angle_approx(dt=0.001)
