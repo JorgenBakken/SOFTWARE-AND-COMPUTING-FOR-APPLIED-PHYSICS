4d863a531826f658088e8daf7be26f70fde711d9
JorgenBakken <jorbak@hotmail.no>
2023-06-13 15:12:57 +0200

    Updating plot_small_angle_approx

--- plotting.py
+++ plotting.py
@@ -1,58 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize
-import scipy.optimize
 
 import help_functions as hf
 
-def plot_small_angle_approx(dt, m, h, IC):
+def plot_small_angle_approx(dt):
+    '''
+    Plot the comparison between the full equation and small angle approximation solutions for a simple harmonic oscillator.
+
+    Inputs:
+    dt : Time step for numerical integration
+
+    Returns:
+    None (displays the plot)
+    '''
+
     # Create subplots for the plots
-    fig, axs = plt.subplots(ncols=2, figsize=(12, 5))
-    
-    # Define initial angular velocity and time range
-    w_0 = np.asarray([20 * np.pi / 180, 0])
-    t_0, t_end = 0, 20
-    
-    # Solve the ODEs using the full equation and small angle approximation
-    t_matrix, w_matrix = hf.solve_ODE(dt, t_0, t_end, w_0, m, h, IC, f=hf.two_component_w_RHS, step_function=hf.euler_step)
-    t_matrix_small_angle, w_matrix_small_angle = hf.solve_ODE(dt, t_0, t_end, w_0, m, h, IC, f=hf.two_component_w_RHS_small_angle, step_function=hf.euler_step)
+    fig, axs = plt.subplots(ncols=2)
+
+    # Define initial angular velocity
+    w_0 = np.asarray([20 * np.pi/180, 0])
+
+    # Large angle
+    # Solve the ODEs using the full equation and small angle approximation 
+    t_matrix, w_matrix = hf.solve_ODE(dt, t_0=0, t_end=20, w_0=w_0, f=hf.two_component_w_RHS, step_function=hf.euler_step)
+    t_matrix_small_angle, w_matrix_small_angle = hf.solve_ODE(dt, t_0=0, t_end=20, w_0=w_0,
+                                        f=hf.two_component_w_RHS_small_angle, step_function=hf.euler_step)
 
     # Plot the comparison for greater angle
-    axs[0].set_title("Comparison for Greater Angle")
     axs[0].set_ylabel(r"$\theta$")
     axs[0].set_xlabel(r"$t$")
-    axs[0].plot(t_matrix, w_matrix[:, 0] * 180 / np.pi, color="blue", linewidth=0.5, label=r"$\theta'' \propto \sin \theta$")
-    axs[0].scatter(t_matrix_small_angle[::150], w_matrix_small_angle[:, 0][::150] * 180 / np.pi, color="red", marker=".", s=10)
+    axs[0].plot(t_matrix, w_matrix[:,0] * 180 /np.pi, color="b", label=r"$\theta'' \propto \sin \theta$")
+    axs[0].scatter(t_matrix_small_angle[::150], w_matrix_small_angle[:,0][::150] * 180 /np.pi, color="r", label=r"$\theta'' \propto \theta$")
 
-    # Define initial angular velocity for smaller angle
-    w_0 = np.asarray([1 * np.pi / 180, 0])
-
-    # Solve the ODEs using the full equation and small angle approximation for smaller angle
-    t_matrix, w_matrix = hf.solve_ODE(dt, t_0, t_end, w_0, m, h, IC, f=hf.two_component_w_RHS, step_function=hf.euler_step)
-    t_matrix_small_angle, w_matrix_small_angle = hf.solve_ODE(dt, t_0, t_end, w_0, m, h, IC, f=hf.two_component_w_RHS_small_angle, step_function=hf.euler_step)
-
+    # Small angle
+    # Solve the ODEs using the full equation and small angle approximation 
+    w_0 = np.asarray([1 * np.pi/180, 0])
+    t_matrix, w_matrix = hf.solve_ODE(dt, t_0=0, t_end=20, w_0=w_0, f=hf.two_component_w_RHS, step_function=hf.euler_step)
+    t_matrix_small_angle, w_matrix_small_angle = hf.solve_ODE(dt, t_0=0, t_end=20, w_0=w_0,
+                                        f=hf.two_component_w_RHS_small_angle, step_function=hf.euler_step)
     # Plot the comparison for smaller angle
-    axs[1].set_title("Comparison for Smaller Angle")
     axs[1].set_ylabel(r"$\theta$")
     axs[1].set_xlabel(r"$t$")
-    axs[1].plot(t_matrix, w_matrix[:, 0] * 180 / np.pi, color="blue", linewidth=0.5)
-    axs[1].scatter(t_matrix_small_angle[::150], w_matrix_small_angle[:, 0][::150] * 180 / np.pi, color="red", marker=".", s=10)
-
+    axs[1].plot(t_matrix, w_matrix[:,0] * 180 /np.pi, color="b")  # , label=r"$\theta'' \propto \sin \theta$")
+    axs[1].scatter(t_matrix_small_angle[::150], w_matrix_small_angle[:,0][::150] * 180 /np.pi, color="r")  # , label=r"$\theta'' \propto \theta$")
     fig.legend(loc="upper center", ncol=2)
-    plt.tight_layout()
+
     plt.show()
-
-
-# Define the constants
-R = 10  # Ship radius (m)
-A_ship = 0.5 * np.pi * R ** 2  # Ship cross-sectional area (m^2)
-sigma = 500  # Ship mass density (kg/m^2)
-m = sigma * A_ship  # Ship mass
-dt = 0.001  # Time step size
-h = 4 * R / (3 * np.pi)  # Distance M - C
-IC = 0.5 * m * R ** 2 * (1 - 32 / (9 * np.pi ** 2)) #The ship's moment of inertia with respect to the axis through C
-
-#plot_small_angle_approx(dt, m, h, IC)
 
 
 def plot_error_step_function(dt_array, theta_0, omega_freq):
