93224758b7746add38342fc39a25cfa6358a8ab9
JorgenBakken <jorbak@hotmail.no>
2023-06-13 13:12:45 +0200

    Implemented calculate_beta

--- help_functions.py
+++ help_functions.py
@@ -1,6 +1,32 @@
 import numpy as np
 import scipy.constants
 
+def calculate_beta(sigma, sigma0):
+    """
+    Calculates the value of beta using the iterative method.
+
+    Inputs:
+        sigma (float): The value of sigma.
+        sigma0 (float): The value of sigma0.
+
+    Returns:
+        float: The calculated value of beta.
+    """
+
+    max_error = 1E-6  # Maximum acceptable error
+    max_iter = int(1E5)  # Maximum number of iterations
+    error = 1.0  # Initialize the error
+    j = 0  # Initialize the iteration counter
+    beta0 = np.pi / 2  # Initial guess for beta
+
+    while error > max_error and j < max_iter:
+        beta1 = np.sin(beta0) + np.pi * sigma / sigma0  # Calculate the next value of beta
+        error = abs(beta1 - beta0)  # Calculate the error
+        beta0 = beta1  # Update the value of beta
+        j += 1  # Increment the iteration counter
+
+    return beta0
+
 def gamma_func(theta, beta, radius, dyC):
     '''
     Calculates the sector angle gamma
