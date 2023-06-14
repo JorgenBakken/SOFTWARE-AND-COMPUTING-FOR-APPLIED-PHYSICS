1229895e15d0cb82d4e2f74871a8e9ee3f86e54a
JorgenBakken <jorbak@hotmail.no>
2023-06-14 14:24:04 +0200

    Updated test_solve_ODE_t0

--- tests.py
+++ tests.py
@@ -208,15 +208,13 @@ def test_solve_ODE_shape_sol_array(dt, t_0, t_end_factor, w_0, fence):
     t_array, w_matrix = hf.solve_ODE(dt, t_0, t_end, w_0, f, hf.RK4_step, fence, sv.kf, sv.omegaW, sv.FW0)
     assert w_matrix.shape == (len(t_array), len(w_0))
 
-@given(dt    = st.floats(min_value=0.000001, max_value=10000000),
-       t_0   = st.floats(min_value=-10000000, max_value=10000000),
-       t_end_factor = st.floats(min_value= 0.000001, max_value=10),
-       w_0   = st.lists(st.floats(min_value=-10000000, max_value=10000000)),
-       m=st.floats(min_value=0.0001, max_value=1000000),
-       h=st.floats(min_value=0.0, max_value=1000),
-       IC=st.floats(min_value=0.000001, max_value=100.0))
+@given(dt=st.floats(min_value=0.000001, max_value=10000000),
+       t_0=st.floats(min_value=-10000000, max_value=10000000),
+       t_end_factor=st.floats(min_value=0.1, max_value=10),
+       w_0=st.lists(st.floats(min_value=-10000000, max_value=10000000), min_size=2, max_size=2),
+       fence = st.booleans())
 @settings(max_examples=50)
-def test_solve_ODE_t0(dt, t_0, t_end_factor, w_0, m, h, IC):
+def test_solve_ODE_t0(dt, t_0, t_end_factor, w_0, fence):
     '''
     Test the solve_ODE function
     Check the first element of the time array
@@ -225,20 +223,20 @@ def test_solve_ODE_t0(dt, t_0, t_end_factor, w_0, m, h, IC):
     dt             : Step length
     t_0            : Start time
     t_end          : End time
-    t_end_factor   : factor to multiply with dt to get t_end, ensures not to many steps 
+    t_end_factor   : Factor to multiply with dt to get t_end, ensures not too many steps 
     w_0            : Initial step
-    m              : Mass of the ship
-    h              : Distance between the midpoint of the deck, M, and the ship's center of mass, C (M - C)
-    IC             : The ship's moment of inertia with respect to the axis through C
+    fence          : Boolean value if the is a fence
 
     Asserts:
     - The first element of the time array matches the start time
     '''
     t_end = t_0 + t_end_factor * dt
-    def f(t, w_old, m, h, IC):
-        return np.sin(t) + w_old
+
+    def f(t, w, fence, kf, omegaW, FW0):
+        # Test function 
+        return np.sin(t) + w
      
-    t_array, w_matrix = hf.solve_ODE(dt, t_0, t_end, w_0, m, h, IC, f, hf.euler_step)
+    t_array, w_matrix = hf.solve_ODE(dt, t_0, t_end, w_0, f, hf.RK4_step, fence, sv.kf, sv.omegaW, sv.FW0)
     assert np.isclose(t_array[0], t_0)
 
 
