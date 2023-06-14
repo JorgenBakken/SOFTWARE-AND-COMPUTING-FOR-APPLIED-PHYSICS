ed375dc15d69273485e2ddb85e1c04a6db5fa217
JorgenBakken <jorbak@hotmail.no>
2023-06-14 19:50:31 +0200

    test_two_component_w_RHS_small_angle_theta_update

--- tests.py
+++ tests.py
@@ -429,29 +429,24 @@ def test_two_component_w_RHS_small_angle_omega_update(t, w, fence):
     # Check if the calculated derivative of omega matches the expected value
     assert np.isclose(updated_w[1], expected_w1)
 
-
 @given(t=st.floats(min_value=0.0, max_value=100.0),
        w=st.lists(st.floats(min_value=-np.pi, max_value=np.pi), min_size=2, max_size=2),
-       m=st.floats(min_value=0.0001, max_value=1000000),
-       h=st.floats(min_value=0.0, max_value=1000),
-       IC=st.floats(min_value=0.000001, max_value=100.0))
-def test_two_component_w_RHS_small_angle_theta_update(t, w, m, h, IC):
+       fence = st.booleans())
+def test_two_component_w_RHS_small_angle_theta_update(t, w, fence):
     '''
     Test for the two_component_w_RHS_small_angle function.
 
     Inputs:
-    t    : Current time
-    w    : Vector [w_0, w_1] representing initial values of theta and omega
-    m    : Mass of the ship
-    h    : Distance between the midpoint of the deck, M, and the ship's center of mass, C (M - C)
-    IC   : The ship's moment of inertia with respect to the axis through C
+    t      : Current time
+    w      : Vector [w_0, w_1] representing initial values of theta and omega
+    fence  : Boolean value if the is a fence
 
     Checks:
     - The calculated derivative of theta is equal to the given value of omega
     '''
 
     # Calculate the derivative using the function
-    updated_w = hf.two_component_w_RHS_small_angle(t, w, m, h, IC)
+    updated_w = hf.two_component_w_RHS_small_angle(t, w, fence)
 
     # Check if the calculated derivative of theta matches the expected value
     assert np.isclose(updated_w[0], w[1]), "updated_w[0] == w[1]"
