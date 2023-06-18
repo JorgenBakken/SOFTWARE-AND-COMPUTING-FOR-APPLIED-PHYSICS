4269ab0077ba247a02d6eec981663830aa7dfa5a
JorgenBakken <jorbak@hotmail.no>
2023-06-18 21:13:16 +0200

    Updated plot_error_step_function

--- main.py
+++ main.py
@@ -9,13 +9,10 @@ import plotting
 # Plotting the comparison between the full equation and small angle approximation for a simple harmonic oscillator
 plotting.plot_small_angle_approx(dt=0.001)
 
-# Defining the parameters for the error plot
-dt_array = np.logspace(-1.5, -3.5, 20)
-theta_0 = 0.01
-omega_freq = np.sqrt(sv.m * sv.g * sv.h / sv.IC)
-
 # Plotting the error as a function of the time step size for Euler and RK4 methods
-plotting.plot_error_step_function(dt_array, theta_0, omega_freq)
+plotting.plot_error_step_function(dt_array = np.logspace(-1.5, -3.5, 20), 
+                                  theta_0 = 0.01, 
+                                  omega_freq = np.sqrt(sv.m * sv.g * sv.h / sv.IC))
 
 # Plotting the ship's x-coordinate and y-coordinate as functions of time
 plotting.plot_ship_coordinates(dt = 0.01, 
